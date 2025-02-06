6880943fa9067c25175bad88c78a79f973167b03
MarcoBruschi <mabruschi02@gmail.com>
2025-02-06 16:40:26 -0300

    Login page project

---
+++ app.js
@@ -0,0 +1,19 @@
+function log() {
+    let inputUser = document.getElementById('user').value;
+    let inputPassword = document.getElementById('password').value;
+    
+    if (inputUser == 'user' && inputPassword == '123') {
+        inputUser = '';
+        inputPassword = '';
+        window.location.href = "logged.html";
+
+    } else {
+        inputUser = '';
+        inputPassword = '';
+        alert(`This user don't exist`);
+    }
+}
+
+function back() {
+    window.location.href = "index.html";
+}
