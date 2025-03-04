fb4b0d7f0db35ce61f0e81dfccb34bd144ae2a23
marco <mabruschi02@gmail.com>
2025-03-04 17:38:59 -0300

    adding some changes

--- app.js
+++ app.js
@@ -1,19 +1,58 @@
-function log() {
-    let inputUser = document.getElementById('user').value;
-    let inputPassword = document.getElementById('password').value;
-    
-    if (inputUser == 'user' && inputPassword == '123') {
-        inputUser = '';
-        inputPassword = '';
-        window.location.href = "logged.html";
+let inputUser = document.getElementById('user');
+let inputPassword = document.getElementById('password');
 
+
+
+function log() {
+    let inputUser = document.getElementById('user');
+    let inputPassword = document.getElementById('password');
+    let errorpassword = document.getElementById("invalid-password-error");
+    let erroremail = document.getElementById("no-email-error");
+
+    
+
+    if (validateEmail(inputUser.value) && inputPassword.value != "") {
+        window.location.href = "logged.html";
     } else {
-        inputUser = '';
-        inputPassword = '';
-        alert(`This user doesn't exist`);
+        errorpassword.style.display = "inline";
+        erroremail.style.display = "inline";
     }
 }
+
+function checkValid() {
+    let errormessage = document.getElementById("invalid-email-error");
+    let inputUser = document.getElementById('user');
+    let errorpassword = document.getElementById("invalid-password-error");
+    let erroremail = document.getElementById("no-email-error");
+
+    if (inputUser.value != "") {
+        erroremail.style.display = "none";
+        if (validateEmail(inputUser.value)) {
+            errormessage.style.display = "none";
+        } else {
+            errormessage.style.display = "inline";
+        }
+    } else {
+        errormessage.style.display = "none";
+    }
+
+    if (inputPassword.value != "") {
+        errorpassword.style.display = "none";
+    }
+}
+
+
+
+
+
+
 
 function back() {
     window.location.href = "index.html";
 }
+
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
